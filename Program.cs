fa52417e640078cf61d958746ca0ae84d303a1d1
Sachin Patney <spatney@microsoft.com>
2019-07-10 20:22:16 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+using System.Security;
+using System;
+
+namespace dotnetinterop
+{
+    class Program
+    {
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(@"./cpp/libsum")]
+        public static extern int Sum(int a, int b);
+        
+        static void Main(string[] args)
+        {
+            var sum = Sum(Int32.Parse(args[0]), Int32.Parse(args[1]));
+
+            Console.WriteLine($"SUM: {sum}");
+        }
+    }
+}
