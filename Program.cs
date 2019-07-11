361237081a95898fbfd0ec42046e4ce603020e7e
Sachin Patney <spatney@microsoft.com>
2019-07-10 21:31:24 -0700

    update

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace dotnetinterop
         [SuppressUnmanagedCodeSecurity]
         [DllImport(@"./cpp/libsum")]
         public static extern int Sum(int a, int b);
-        
+
         static void Main(string[] args)
         {
             var sum = Sum(Int32.Parse(args[0]), Int32.Parse(args[1]));
